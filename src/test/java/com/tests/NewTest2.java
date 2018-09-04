d0aaa2b03896bc76202e3b461ed4604a2213dbd0
Training_C2a.05.07 <Training_C2a.05.07@accenture.com>
2018-09-04 12:02:05 +0530

    new

--- src/test/java/com/tests/NewTest2.java
+++ src/test/java/com/tests/NewTest2.java
@@ -25,7 +25,7 @@ public class NewTest2 {
 	@Test
 	public void launchfirefox()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\Software\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\Software\chromedriver.exe");
 	    	    driver=new ChromeDriver();
 		driver.get("http://newtours.demoaut.com/");
 		driver.manage().window().maximize();
